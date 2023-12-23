0d4117e0bc0e5601083a3ee0efa467c890dad1da
Melikyan-Nana <114040581+Melikyan-Nana@users.noreply.github.com>
2023-12-23 19:37:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from flask import Flask
 from region_routes import region_routes
 from tax_param_route import tax_param
 from tax_route import tax_route
-from db_test import db
+from tables import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/RPP_Lab4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/rpp_lab_4'
 app.register_blueprint(region_routes)
 app.register_blueprint(tax_param)
 app.register_blueprint(tax_route)
 
-app.config['SQLAlchemy_TRACK_MODIFIVATTION'] = False
+app.config['SQLALCHEMY_TRACK_MODIFIVATTION'] = False
 db.init_app(app)
 
 
